a7b77deaac9f1a6bec6baa764e348569878b295d
Runster <Runster@users.noreply.github.com>
2015-05-24 23:28:23 +0200

    Added a function to give the calendar your own style

--- calendar/calendar.php
+++ calendar/calendar.php
@@ -3,147 +3,171 @@
 class Calendar
 {
 
-    private $time;
+	private $time;
 
-    private $output = "";
+	private $output = "";
 
-    private $firstline;
+	private $firstline;
 
-    private $showOnlyDaysOfThisMonth = false;
+	private $showOnlyDaysOfThisMonth = false;
 
-    private $weekdays = array(
-            "Mon" => "Monday",
-            "Tue" => "Tuesday",
-            "Wed" => "Wednesday",
-            "Thu" => "Thursday",
-            "Fri" => "Friday",
-            "Sat" => "Saturday",
-            "Sun" => "Sunday"
-    );
+	private $weekdays = array();
 
-    private $errorList = array();
+	private $errorList = array();
 
-    public function __construct ()
-    {
-        $this->time = time();
-        $this->firstline = true;
-    }
+	private $styleClasses = array();
 
-    public function generateFullCalendar ()
-    {
-        $daysMonth = date("t", $this->time);
-        $monthStartTimestamp = mktime(0, 0, 0, date("n", $this->time), 1, 
-                date("Y", $this->time));
-        $monthStartNumber = date("N", $monthStartTimestamp);
-        $monthEndNumber = date("N", 
-                mktime(0, 0, 0, date("n", $this->time), $daysMonth, 
-                        date("Y", $this->time)));
-        $this->createTableHeader();
-        for ($i = 1; $i <=
-                 $daysMonth + ($monthStartNumber - 1) + (7 - $monthEndNumber); $i ++) {
-            $newline = false;
-            $currectDay = $i - $monthStartNumber;
-            $currentDayTimestamp = strtotime($currectDay . " day", 
-                    $monthStartTimestamp);
-            $dayOfMonthOutput = date("j", $currentDayTimestamp);
-            if (date("N", $currentDayTimestamp) == 1) {
-                $newline = true;
-            }
-            if (floor(($this->time - $currentDayTimestamp) / (3600 * 24)) == 0) {
-                // Current day is the real current day
-                $this->addTableCell($dayOfMonthOutput, $newline);
-            } elseif ($currectDay >= 0 and $currectDay < $daysMonth) {
-                // Just another day in current month
-                $this->addTableCell($dayOfMonthOutput, $newline);
-            } else {
-                // Day in another month
-                if (! $this->showOnlyDaysOfThisMonth) {
-                    $this->addTableCell($dayOfMonthOutput, $newline);
-                } else {
-                    $this->addTableCell("", $newline);
-                }
-            }
-        }
-        $this->createTableFooter();
-    }
+	public function __construct ()
+	{
+		$this->time = time();
+		$this->firstline = true;
+		$this->initWeekdays();
+	}
 
-    private function createTableHeader ()
-    {
-        $this->output .= "<table>\n";
-        $this->output .= "\t<caption>" . date("F Y", $this->time) .
-                 "</caption>\n";
-        $this->output .= "\t<thead>\n";
-        $this->output .= "\t\t<tr>\n";
-        foreach ($this->weekdays as $weekdaysShort => $weekdaysLong) {
-            $this->output .= "\t\t\t<td>" . $weekdaysShort . "</td>\n";
-        }
-        $this->output .= "\t\t</tr>\n";
-        $this->output .= "\t</thead>\n";
-        $this->output .= "\t<tbody>\n";
-    }
+	public function generateFullCalendar ()
+	{
+		$daysMonth = date("t", $this->time);
+		$monthStartTimestamp = mktime(0, 0, 0, date("n", $this->time), 1, 
+				date("Y", $this->time));
+		$monthStartNumber = date("N", $monthStartTimestamp);
+		$monthEndNumber = date("N", 
+				mktime(0, 0, 0, date("n", $this->time), $daysMonth, 
+						date("Y", $this->time)));
+		$this->createTableHeader();
+		for ($i = 1; $i <=
+				 $daysMonth + ($monthStartNumber - 1) + (7 - $monthEndNumber); $i ++) {
+			$newline = false;
+			$currectDay = $i - $monthStartNumber;
+			$currentDayTimestamp = strtotime($currectDay . " day", 
+					$monthStartTimestamp);
+			$dayOfMonthOutput = date("j", $currentDayTimestamp);
+			if (date("N", $currentDayTimestamp) == 1) {
+				$newline = true;
+			}
+			if (floor(($this->time - $currentDayTimestamp) / (3600 * 24)) == 0) {
+				// Current day in the loop is the real current day
+				$this->addTableCell($dayOfMonthOutput, $newline, "current_day");
+			} elseif ($currectDay >= 0 && $currectDay < $daysMonth) {
+				// Just another day in current month
+				$this->addTableCell($dayOfMonthOutput, $newline);
+			} else {
+				// Day in another month
+				if (! $this->showOnlyDaysOfThisMonth) {
+					$this->addTableCell($dayOfMonthOutput, $newline, 
+							"last_month_days");
+				} else {
+					$this->addTableCell("", $newline);
+				}
+			}
+		}
+		$this->createTableFooter();
+	}
 
-    private function createTableFooter ()
-    {
-        $this->output .= "\t</tbody>\n";
-        $this->output .= "</table>";
-    }
+	private function initWeekdays ()
+	{
+		$timestamp = strtotime('next Monday');
+		for ($i = 0; $i < 7; $i ++) {
+			$this->weekdays[strftime('%a', $timestamp)] = strftime('%A', 
+					$timestamp);
+			$timestamp = strtotime('+1 day', $timestamp);
+		}
+	}
 
-    private function addTableCell ($input, $newline)
-    {
-        if ($newline && ! $this->firstline) {
-            $this->output .= "\t\t</tr>\n";
-        }
-        
-        if ($newline) {
-            $this->output .= "\t\t<tr>\n";
-        }
-        
-        $this->output .= "\t\t\t<td>" . $input . "</td>\n";
-        
-        $this->firstline = false;
-    }
+	private function createTableHeader ()
+	{
+		$this->output .= "<table" . $this->checkForStyleClass("table") . ">\n";
+		$this->output .= "\t<caption>" . date("F Y", $this->time) .
+				 "</caption>\n";
+		$this->output .= "\t<thead>\n";
+		$this->output .= "\t\t<tr>\n";
+		foreach ($this->weekdays as $weekdaysShort => $weekdaysLong) {
+			$this->output .= "\t\t\t<td>" . $weekdaysShort . "</td>\n";
+		}
+		$this->output .= "\t\t</tr>\n";
+		$this->output .= "\t</thead>\n";
+		$this->output .= "\t<tbody>\n";
+	}
 
-    public function setWeekdays ($weekdaysArray)
-    {
-        if (count($weekdaysArray) == 7) {
-            $this->weekdays = $weekdaysArray;
-        } else {
-            $this->errorList["Wrong size array"] = "Your specified weekday-array hasn't the size of seven items.";
-        }
-    }
+	private function createTableFooter ()
+	{
+		$this->output .= "\t\t</tr>\n";
+		$this->output .= "\t</tbody>\n";
+		$this->output .= "</table>";
+	}
 
-    public function output ()
-    {
-        return $this->output;
-    }
+	private function addTableCell ($input, $newline, $class = null)
+	{
+		if ($newline && ! $this->firstline) {
+			$this->output .= "\t\t</tr>\n";
+		}
+		
+		if ($newline) {
+			$this->output .= "\t\t<tr>\n";
+		}
+		
+		if ($class === null) {
+			$this->output .= "\t\t\t<td>" . $input . "</td>\n";
+		} else {
+			$this->output .= "\t\t\t<td" . $this->checkForStyleClass($class) .
+					 ">" . $input . "</td>\n";
+		}
+		
+		$this->firstline = false;
+	}
 
-    public function showOnlyDaysOfThisMonth ($boolean)
-    {
-        $this->showOnlyDaysOfThisMonth = $this->getBoolean($boolean, 
-                $this->showOnlyDaysOfThisMonth);
-    }
+	public function setWeekdays ($weekdaysArray)
+	{
+		if (count($weekdaysArray) == 7) {
+			$this->weekdays = $weekdaysArray;
+		} else {
+			$this->errorList["Wrong size array"] = "Your specified weekday-array hasn't the size of seven items.";
+		}
+	}
 
-    private function getBoolean ($booleanToCheck, $default)
-    {
-        if (is_bool($booleanToCheck)) {
-            return $booleanToCheck;
-        } elseif (strcasecmp($booleanToCheck, "true") == 0) {
-            return true;
-        } elseif (strcasecmp($booleanToCheck, "false") == 0) {
-            return false;
-        } else {
-            $this->errorList["Parse error"] = "One of your parameters isn't a valid boolean.";
-        }
-        
-        return $default;
-    }
+	public function output ()
+	{
+		return $this->output;
+	}
 
-    public function getErrors ()
-    {
-        $error_output = "";
-        foreach ($this->errorList as $error) {
-            $error_output .= "<p>" . $error . "</p>";
-        }
-        return $error_output;
-    }
+	public function showOnlyDaysOfThisMonth ($boolean)
+	{
+		$this->showOnlyDaysOfThisMonth = $this->getBoolean($boolean, 
+				$this->showOnlyDaysOfThisMonth);
+	}
+
+	private function getBoolean ($booleanToCheck, $default)
+	{
+		if (is_bool($booleanToCheck)) {
+			return $booleanToCheck;
+		} elseif (strcasecmp($booleanToCheck, "true") == 0) {
+			return true;
+		} elseif (strcasecmp($booleanToCheck, "false") == 0) {
+			return false;
+		} else {
+			$this->errorList["Parse error"] = "One of your parameters isn't a valid boolean.";
+		}
+		
+		return $default;
+	}
+
+	public function getErrors ()
+	{
+		$error_output = "";
+		foreach ($this->errorList as $error) {
+			$error_output .= "<p>" . $error . "</p>";
+		}
+		return $error_output;
+	}
+
+	private function checkForStyleClass ($tagName)
+	{
+		if (array_key_exists($tagName, $this->styleClasses)) {
+			return " class=\"" . $this->styleClasses[$tagName] . "\"";
+		}
+	}
+
+	public function addStyleClasses ($styleClasses)
+	{
+		$this->styleClasses = $styleClasses;
+	}
 }
